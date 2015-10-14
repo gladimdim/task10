3d7b098254df54844b70792267ff94afc0955745
Dmytro Gladkyi <dmytro.gladkyi@sap.com>
2015-10-14 22:08:28 +0200

    Moved $route.when object handler to separate function.
    First tab is init by default.

--- js/main.js
+++ js/main.js
@@ -10,51 +10,38 @@ define(["angular", "angularRoute"], function(angular, ngRoute) {
     app.run(function($route, $rootScope, $http) {
         $http.get("data.json").success(function(response) {
             //lets rearrange tabs based on their order
-            $rootScope.tabs = response.sort(function (a, b) {
+            $rootScope.tabs = response.sort(function(a, b) {
                 return a.order - b.order;
             });
-	    //init with first tab
-	    $routeProviderRef.when("/", {
-		template: "<div ng-bind-html='renderUnsafeHtml(this.viewHTML);'>{{this.viewHTML}}</div>",
-		controller: "routeController",
-		resolve: {
-		    "Tabs": ["$q", function($q) {
-			var deferred = $q.defer();
-			require([response[0].path], function(module) {
-			    deferred.resolve(module);
-			});
-			return deferred.promise;
-		    }
-		    ]
-		}
-	    });
-            response.forEach(function(el) {
-                $routeProviderRef
-                .when("/" + el.id, {
-                    template: function() {
-                        return "<div ng-bind-html='renderUnsafeHtml(this.viewHTML);'>{{this.viewHTML}}</div>";
-                    },
+            var oWhenProcessor = function(sPathToModule) {
+                return {
+                    template: "<div ng-bind-html='renderUnsafeHtml(this.viewHTML);'>{{this.viewHTML}}</div>",
                     controller: "routeController",
                     resolve: {
                         "Tabs": ["$q", function($q) {
                             var deferred = $q.defer();
-                            require([el.path], function(module) {
+                            require([sPathToModule], function(module) {
                                 deferred.resolve(module);
                             });
                             return deferred.promise;
                         }]
                     }
-                });
+                }
+            };
+            //init with first tab
+            $routeProviderRef.when("/", oWhenProcessor(response[0].path));
+            response.forEach(function(el) {
+                $routeProviderRef
+                    .when("/" + el.id, oWhenProcessor(el.path));
             });
-            
+
             $route.reload();
         });
     });
 
 
 
-    app.controller("cmsController", function($scope, $http, $route) {
-    });
+    app.controller("cmsController", function($scope, $http, $route) {});
 
     app.controller("routeController", function($scope, Tabs, $sce) {
         $scope.viewHTML = Tabs.render().el.outerHTML;
